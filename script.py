c21308633ac5c436e793eb4cbfa69a8370b97cef
mariuszpozarlik <pozarek89@gmail.com>
2025-06-11 09:54:37 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from multiprocessing import Process, Pool
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def fun(x):
+    while(1):
+        print(x)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    p1 = Process(target=fun, args=("1"))
+    p2 = Process(target=fun, args=("2"))
+    p3 = Process(target=fun, args=("3"))
+    p4 = Process(target=fun, args=("4"))
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
